33f6f378e8ad483656e761676b99b7c7928b7e61
Joshua Bell <belljos@oregonstate.edu>
2019-07-11 17:24:31 -0700

    Add project files.

---
+++ Model/Book.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookInventory.Model
+{
+    public class Book : INotifyPropertyChanged
+    {
+        private string title;
+        private string author;
+        private int stock;
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+
+            set
+            {
+                if (title != value)
+                {
+                    title = value;
+                    RaisePropertyChanged("Title");
+                }
+            }
+        }
+
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+
+            set
+            {
+                if (author != value)
+                {
+                    author = value;
+                    RaisePropertyChanged("Author");
+                }
+            }
+        }
+
+        public int Stock
+        {
+            get
+            {
+                return stock;
+            }
+
+            set
+            {
+                if (stock != value)
+                {
+                    stock = value;
+                    RaisePropertyChanged("Stock");
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+    }
+}
---
+++ View Model/BookViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookInventory.Model;
+
+namespace BookInventory.View_Model
+{
+    public class BookViewModel
+    {
+        public ObservableCollection<Book> Books
+        {
+            get;
+            set;
+        }
+
+        public void LoadBooks()
+        {
+            ObservableCollection<Book> books = new ObservableCollection<Book>();
+
+            books.Add(new Book { Title = "The Cat in the Hat", Author = "Dr. Seuss", Stock = 5 });
+            books.Add(new Book { Title = "The Hobbit", Author = "J. R. R. Tolkien", Stock = 3 });
+            books.Add(new Book { Title = "Harry Potter and the Philosopher's Stone", Author = "J. K. Rowling", Stock = 8 });
+
+            Books = books;
+        }
+
+        public void AddBook(string title, string author, int stock)
+        {
+            Books.Add(new Book { Title = title, Author = author, Stock = stock });
+        }
+
+        public void RemoveBook(string title, string author)
+        {
+            foreach (Book book in Books)
+            {
+                if (book.Title == title && book.Author == author) 
+                {
+                    Books.Remove(book);
+                }
+            }
+        }
+    }
+}
